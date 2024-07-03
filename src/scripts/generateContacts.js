505a3096abffa058d70a0972e554c6f881262152
NoretsAndrii <147941173+NoretsAndrii@users.noreply.github.com>
2024-07-03 11:41:38 +0300

    create generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,24 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  const newContacts = [];
+  for (let i = 1; i <= number; i += 1) {
+    newContacts.push(createFakeContact());
+  }
+  try {
+    const conractsDataJSON = await fs.readFile(PATH_DB, 'utf8');
+    const conractsData = conractsDataJSON.length
+      ? JSON.parse(conractsDataJSON)
+      : [];
+    const newContactsData = [...conractsData, ...newContacts];
+
+    await fs.writeFile(PATH_DB, JSON.stringify(newContactsData), 'utf8');
+    console.log('Дані успішно додані до файлу.');
+  } catch (error) {
+    console.error('Помилка додавання даних до файлу:', error);
+  }
+};
 
 generateContacts(5);
